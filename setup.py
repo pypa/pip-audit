59fe661bfeaffbe54caea485e7d4d59e86e59232
William Woodruff <william@trailofbits.com>
2021-09-09 10:32:12 -0400

    setup: make dependency on packaging explicit

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
     python_requires=">=3.6",
     install_requires=[
         "pip-api==0.0.20",
+        "packaging==21.0.0",
         # TODO: Remove this once 3.7 is our minimally supported version.
         "dataclasses==0.6",
     ],
