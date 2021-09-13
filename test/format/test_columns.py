236e6c5c94ff838d6f43dad53ea543fa2a6124aa
Alex Cameron <asc@tetsuo.sh>
2021-09-13 21:12:25 +1000

    format: separate vulnerability test data into separate module

--- test/format/test_columns.py
+++ test/format/test_columns.py
@@ -1,42 +1,13 @@
-from typing import Dict, List
-
-from packaging.version import Version
-
 import pip_audit.format as format
-import pip_audit.service as service
+
+from .vuln_data import TEST_VULN_DATA
 
 
 def test_columns():
     columns_format = format.ColumnsFormat()
-    result: Dict[service.Dependency, List[service.VulnerabilityResult]] = {
-        service.Dependency(package="foo", version="1.0"): [
-            service.VulnerabilityResult(
-                id="VULN-0",
-                description="The first vulnerability",
-                version_range=[
-                    service.VersionRange(introduced=Version("0.9"), fixed=Version("1.1")),
-                    service.VersionRange(introduced=Version("1.3"), fixed=Version("1.4")),
-                ],
-            ),
-            service.VulnerabilityResult(
-                id="VULN-1",
-                description="The second vulnerability",
-                version_range=[
-                    service.VersionRange(introduced=Version("0.5"), fixed=Version("1.0"))
-                ],
-            ),
-        ],
-        service.Dependency(package="bar", version="0.1"): [
-            service.VulnerabilityResult(
-                id="VULN-2",
-                description="The third vulnerability",
-                version_range=[service.VersionRange(introduced=Version("0.1"), fixed=None)],
-            )
-        ],
-    }
     expected_columns = """Package Version ID     Description              Affected Versions
 ------- ------- ------ ------------------------ ------------------------------------
-foo     1.0     VULN-0 The first vulnerability  (0.9 => fix: 1.1), (1.3 => fix: 1.4)
+foo     1.0     VULN-0 The first vulnerability  (0.9 => fix: 1.1), (N/A => fix: 1.4)
 foo     1.0     VULN-1 The second vulnerability (0.5 => fix: 1.0)
 bar     0.1     VULN-2 The third vulnerability  (0.1 => fix: N/A)"""
-    assert columns_format.format(result) == expected_columns
+    assert columns_format.format(TEST_VULN_DATA) == expected_columns
--- test/format/test_json.py
+++ test/format/test_json.py
@@ -1,40 +1,12 @@
 import json
-from typing import Dict, List
-
-from packaging.version import Version
 
 import pip_audit.format as format
-import pip_audit.service as service
+
+from .vuln_data import TEST_VULN_DATA
 
 
 def test_json():
     json_format = format.JsonFormat()
-    result: Dict[service.Dependency, List[service.VulnerabilityResult]] = {
-        service.Dependency(package="foo", version="1.0"): [
-            service.VulnerabilityResult(
-                id="VULN-0",
-                description="The first vulnerability",
-                version_range=[
-                    service.VersionRange(introduced=Version("0.9"), fixed=Version("1.1")),
-                    service.VersionRange(introduced=Version("1.3"), fixed=Version("1.4")),
-                ],
-            ),
-            service.VulnerabilityResult(
-                id="VULN-1",
-                description="The second vulnerability",
-                version_range=[
-                    service.VersionRange(introduced=Version("0.5"), fixed=Version("1.0"))
-                ],
-            ),
-        ],
-        service.Dependency(package="bar", version="0.1"): [
-            service.VulnerabilityResult(
-                id="VULN-2",
-                description="The third vulnerability",
-                version_range=[service.VersionRange(introduced=Version("0.1"), fixed=None)],
-            )
-        ],
-    }
     expected_json = [
         {
             "package": "foo",
@@ -45,7 +17,7 @@ def test_json():
                     "description": "The first vulnerability",
                     "version_range": [
                         {"introduced": "0.9", "fixed": "1.1"},
-                        {"introduced": "1.3", "fixed": "1.4"},
+                        {"fixed": "1.4"},
                     ],
                 },
                 {
@@ -69,4 +41,4 @@ def test_json():
             ],
         },
     ]
-    assert json_format.format(result) == json.dumps(expected_json)
+    assert json_format.format(TEST_VULN_DATA) == json.dumps(expected_json)
