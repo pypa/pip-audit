ac703d0471f1c3184aa60d165b9596ef4fe26130
William Woodruff <william@trailofbits.com>
2021-09-14 14:37:50 -0400

    pip_audit/util: make coverage happy
    This is impossible to test.

--- pip_audit/util.py
+++ pip_audit/util.py
@@ -1,8 +1,8 @@
-from typing import NoReturn
+from typing import NoReturn  # pragma: no cover
 
 
-def assert_never(x: NoReturn) -> NoReturn:
+def assert_never(x: NoReturn) -> NoReturn:  # pragma: no cover
     """
     A hint to the typechecker that a branch can never occur.
     """
-    assert False, f"unhandled type: {type(x).__name__}"  # pragma: no cover
+    assert False, f"unhandled type: {type(x).__name__}"
