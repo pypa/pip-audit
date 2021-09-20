e1cf4282d4d544cd39b0d26e9264d7dfa88b356e
Alex Cameron <asc@tetsuo.sh>
2021-09-20 16:54:50 +1000

    test: use `flask==1.0.1` to avoid different dependencies on different Python versions

--- test/dependency_source/test_resolvelib.py
+++ test/dependency_source/test_resolvelib.py
@@ -25,15 +25,15 @@ def get_metadata_mock():
 
 def test_resolvelib():
     resolver = resolvelib.ResolveLibResolver()
-    req = Requirement("flask==2.0.1")
+    req = Requirement("flask==1.0.1")
     resolved_deps = dict(resolver.resolve_all([req]))
     assert len(resolved_deps) == 1
     expected_deps = [
-        Dependency("flask", Version("2.0.1")),
-        Dependency("werkzeug", Version("2.0.1")),
-        Dependency("jinja2", Version("3.0.1")),
+        Dependency("flask", Version("1.0.1")),
         Dependency("itsdangerous", Version("2.0.1")),
+        Dependency("jinja2", Version("3.0.1")),
         Dependency("click", Version("8.0.1")),
+        Dependency("werkzeug", Version("2.0.1")),
         Dependency("markupsafe", Version("2.0.1")),
     ]
     assert req in resolved_deps
