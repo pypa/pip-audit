85f76160bb1ed380ae341968482036c36e081a53
Alex Cameron <asc@tetsuo.sh>
2021-10-08 04:23:10 +1100

    Add a `RequirementSource` to our dependency source API (#51)
    * Add parsing for sdist filenames
    
    * Add helper to extract metadata from source tarball
    
    * Fix filename parsing
    
    * Remove any docs or wording that imply that the provider only supports wheels
    
    * Prioritize wheels
    
    * {pip_audit,test}/dependency_source: simplifications
    
    * Add basic logic for installing sdist under a virtual env and
    inspecting the packages
    
    * Add `virtualenv-api` dependency
    
    * Recreate metadata based on installed packages and fix unit test
    
    * Stop hardcoding the package name
    
    * Add virtualenv as a dependency since it needs to be in the path for
    the API package to work
    
    * Remove seed packages
    
    * Add support for extras in `PyPIProvider` (#47)
    
    * Add support for extras in dependency resolver
    
    * Add unit test for resolving dependencies in extras
    
    * Remove todo comment
    
    * Use temporary directories for virtual environments and installation paths
    
    * Use `raise_for_status` to check for HTTP errors
    
    * Add `RequirementSource`
    
    * Support `-r` flag in the CLI
    
    * Fix indentation
    
    * Error handling
    
    * Add basic unit test for `RequirementSource`
    
    * test: modify tests to check for a minimum version
    
    * Terminate iteration if we find a matching dependency
    
    * test: fill in remaining coverage
    
    * test: add comment elaborating on why we expect a parsing error
    
    * Use `resolve_all` function
    
    * test: add test for multiple requirements files
    
    * Deduplicate dependencies
    
    Co-authored-by: William Woodruff <william@trailofbits.com>

---
+++ pip_audit/dependency_source/requirement.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+from typing import Iterator, List, Set
+
+from packaging.requirements import Requirement
+from pip_api import parse_requirements
+from pip_api.exceptions import PipError
+
+from pip_audit.dependency_source import (
+    DependencyResolver,
+    DependencyResolverError,
+    DependencySource,
+    DependencySourceError,
+)
+from pip_audit.service import Dependency
+
+
+class RequirementSource(DependencySource):
+    def __init__(self, filenames: List[Path], resolver: DependencyResolver):
+        self.filenames = filenames
+        self.resolver = resolver
+
+    def collect(self) -> Iterator[Dependency]:
+        collected: Set[Dependency] = set()
+        for filename in self.filenames:
+            try:
+                reqs = parse_requirements(filename=filename)
+            except PipError as pe:
+                raise RequirementSourceError("requirement parsing raised an error") from pe
+
+            # Invoke the dependency resolver to turn requirements into dependencies
+            req_values: List[Requirement] = [Requirement(str(req)) for req in reqs.values()]
+            try:
+                for _, deps in self.resolver.resolve_all(iter(req_values)):
+                    for dep in deps:
+                        # Don't allow duplicate dependencies to be returned
+                        if dep in collected:
+                            continue
+                        collected.add(dep)
+                        yield dep
+            except DependencyResolverError as dre:
+                raise RequirementSourceError("dependency resolver raised an error") from dre
+
+
+class RequirementSourceError(DependencySourceError):
+    pass
---
+++ test/dependency_source/test_requirement.py
@@ -0,0 +1,96 @@
+from pathlib import Path
+from typing import List
+
+import pytest
+from packaging.requirements import Requirement
+from packaging.version import Version
+from pip_api import _parse_requirements
+
+from pip_audit.dependency_source import (
+    DependencyResolver,
+    DependencyResolverError,
+    DependencySourceError,
+    ResolveLibResolver,
+    requirement,
+)
+from pip_audit.service import Dependency
+
+
+def test_requirement_source(monkeypatch):
+    source = requirement.RequirementSource([Path("requirements.txt")], ResolveLibResolver())
+
+    monkeypatch.setattr(_parse_requirements, "_read_file", lambda _: ["flask==2.0.1"])
+
+    specs = list(source.collect())
+    assert Dependency("flask", Version("2.0.1")) in specs
+
+
+def test_requirement_source_multiple_files(monkeypatch):
+    file1 = "requirements1.txt"
+    file2 = "requirements2.txt"
+    file3 = "requirements3.txt"
+
+    source = requirement.RequirementSource(
+        [Path(file1), Path(file2), Path(file3)],
+        ResolveLibResolver(),
+    )
+
+    def read_file_mock(f):
+        filename = f.name
+        if filename == file1:
+            return ["flask==2.0.1"]
+        elif filename == file2:
+            return ["requests==2.8.1"]
+        else:
+            assert filename == file3
+            return ["pip-api==0.0.22\n", "packaging==21.0"]
+
+    monkeypatch.setattr(_parse_requirements, "_read_file", read_file_mock)
+
+    specs = list(source.collect())
+    assert Dependency("flask", Version("2.0.1")) in specs
+    assert Dependency("requests", Version("2.8.1")) in specs
+    assert Dependency("pip-api", Version("0.0.22")) in specs
+    assert Dependency("packaging", Version("21.0")) in specs
+
+
+def test_requirement_source_parse_error(monkeypatch):
+    source = requirement.RequirementSource([Path("requirements.txt")], ResolveLibResolver())
+
+    # Duplicate dependencies aren't allowed in a requirements file so we should expect the parser to
+    # raise here
+    monkeypatch.setattr(
+        _parse_requirements, "_read_file", lambda _: ["flask==2.0.1\n", "flask==2.0.0"]
+    )
+
+    with pytest.raises(DependencySourceError):
+        list(source.collect())
+
+
+def test_requirement_source_resolver_error(monkeypatch):
+    # Pass the requirement source a resolver that automatically raises errors
+    class MockResolver(DependencyResolver):
+        def resolve(self, req: Requirement) -> List[Dependency]:
+            raise DependencyResolverError
+
+    source = requirement.RequirementSource([Path("requirements.txt")], MockResolver())
+
+    monkeypatch.setattr(_parse_requirements, "_read_file", lambda _: ["flask==2.0.1"])
+
+    with pytest.raises(DependencySourceError):
+        list(source.collect())
+
+
+def test_requirement_source_duplicate_dependencies(monkeypatch):
+    source = requirement.RequirementSource(
+        [Path("requirements1.txt"), Path("requirements2.txt")], ResolveLibResolver()
+    )
+
+    # Return the same requirements for both files
+    monkeypatch.setattr(_parse_requirements, "_read_file", lambda _: ["flask==2.0.1"])
+
+    specs = list(source.collect())
+
+    # If the dependency list has duplicates, then converting to a set will reduce the length of the
+    # collection
+    assert len(specs) == len(set(specs))
