525c53d127d82ca700335efffb3ae7d164233c93
Alex Cameron <asc@tetsuo.sh>
2021-10-08 19:57:58 +1100

    Switch to using JSON `pip list` format instead

--- pip_audit/virtual_env.py
+++ pip_audit/virtual_env.py
@@ -2,9 +2,9 @@
 Create virtual environments with a custom set of packages and inspect their dependencies.
 """
 
+import json
 import subprocess
 import venv
-from os import linesep
 from typing import Iterator, List, Optional, Tuple
 
 from packaging.version import Version
@@ -24,22 +24,16 @@ class VirtualEnv(venv.EnvBuilder):
             cmd.extend(install_cmd.split())
             subprocess.check_output(cmd, stderr=subprocess.STDOUT)
 
-        # Now parse the `pip freeze` output to figure out what packages our
+        # Now parse the `pip list` output to figure out what packages our
         # environment contains
-        freeze_cmd = [context.env_exe, "-m", "pip", "freeze", "-l"]
-        freeze_output = subprocess.check_output(freeze_cmd).decode("utf-8")
+        list_cmd = [context.env_exe, "-m", "pip", "list", "-l", "--format", "json"]
+        list_output = subprocess.check_output(list_cmd).decode("utf-8")
+        package_list = json.loads(list_output)
 
         # Convert into a series of name, version pairs
         self.packages = []
-        lines = freeze_output.split(linesep)
-        for line in lines:
-            # Skip source distributions and comments
-            if not line or line.startswith("#") or line.startswith("-e"):
-                continue  # pragma: no cover
-            parts = line.split("==")
-            if len(parts) != 2:
-                raise VirtualEnvError(f"Malformed line in `pip freeze` output: {line}")
-            self.packages.append((parts[0], Version(parts[1])))
+        for package in package_list:
+            self.packages.append((package["name"], Version(package["version"])))
 
     @property
     def installed_packages(self) -> Iterator[Tuple[str, Version]]:
@@ -48,8 +42,8 @@ class VirtualEnv(venv.EnvBuilder):
                 "Invalid usage of wrapper."
                 "The `create` method must be called before inspecting `installed_packages`."
             )
-        for name, version in self.packages:
-            yield (name, version)
+        for package in self.packages:
+            yield package
 
 
 class VirtualEnvError(Exception):
--- test/test_virtual_env.py
+++ test/test_virtual_env.py
@@ -1,4 +1,3 @@
-import subprocess
 from tempfile import TemporaryDirectory
 
 import pytest
@@ -20,20 +19,3 @@ def test_virtual_env_incorrect_usage():
 
     with pytest.raises(VirtualEnvError):
         list(ve.installed_packages)
-
-
-def test_virtual_env_malformed_freeze_output(monkeypatch):
-    original_check_output = subprocess.check_output
-
-    def check_output_mock(args, **kwargs):
-        if "freeze" in args:
-            return str.encode("Flask!=2.0.1")
-        # If it's not a call to `pip freeze` just forward the args on
-        return original_check_output(args, **kwargs)
-
-    monkeypatch.setattr(subprocess, "check_output", check_output_mock)
-
-    with TemporaryDirectory() as ve_dir:
-        ve = VirtualEnv(["flask==2.0.1"])
-        with pytest.raises(VirtualEnvError):
-            ve.create(ve_dir)
