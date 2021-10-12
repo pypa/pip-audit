605aa3852ad1bafb9ed98b703a3cc1b0bccc2d08
William Woodruff <william@trailofbits.com>
2021-10-12 13:27:15 -0400

    pip_audit: support for filtering system dependencies
    Closes #7.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     platforms="any",
     python_requires=">=3.6",
     install_requires=[
-        "pip-api>=0.0.22",
+        "pip-api>=0.0.23",
         "packaging>=21.0.0",
         # TODO: Remove this once 3.7 is our minimally supported version.
         "dataclasses>=0.6",
