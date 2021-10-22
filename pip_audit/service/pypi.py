2a2a1cf7b8e616cdd015d051022ad9270688427e
Alex Cameron <asc@tetsuo.sh>
2021-10-22 17:40:48 +1100

    pip_audit: Add ETags-based caching to the PyPI vulnerability service

--- pip_audit/service/pypi.py
+++ pip_audit/service/pypi.py
@@ -1,23 +1,56 @@
-from typing import List
+import logging
+from dataclasses import dataclass
+from typing import Any, Dict, List
 
 import requests
 from packaging.version import InvalidVersion, Version
 
 from .interface import Dependency, ServiceError, VulnerabilityResult, VulnerabilityService
 
+logger = logging.getLogger(__name__)
+
+
+@dataclass()
+class CachedVulnerabilityData:
+    etag: str
+    response_json: Dict[str, Any]
+
 
 class PyPIService(VulnerabilityService):
+    def __init__(self):
+        self.cache: Dict[Dependency, CachedVulnerabilityData] = {}
+
     def query(self, spec: Dependency) -> List[VulnerabilityResult]:
+        cached_data = None
+        headers = None
+        if spec in self.cache:
+            cached_data = self.cache[spec]
+            headers = {"If-None-Match": cached_data.etag}
+
         url = f"https://pypi.org/pypi/{spec.package}/{str(spec.version)}/json"
-        response: requests.Response = requests.get(url=url)
+        response: requests.Response = requests.get(url=url, headers=headers)
         try:
             response.raise_for_status()
         except requests.HTTPError as http_error:
             raise ServiceError from http_error
 
-        response_json = response.json()
-        results: List[VulnerabilityResult] = []
+        if response.status_code == 304:
+            if cached_data is None:
+                raise ServiceError(
+                    f'Received "not modified" response without specifying ETag: {response}'
+                )
+            logger.debug(f"Using cached entry, cached={cached_data}")
+            response_json = cached_data.response_json
+        else:
+            response_json = response.json()
+            new_entry = CachedVulnerabilityData(response.headers["ETag"], response_json)
+            if spec in self.cache:
+                logger.debug(
+                    f"Overwriting existing cache entry, old={self.cache[spec]}, new={new_entry}"
+                )
+            self.cache[spec] = new_entry
 
+        results: List[VulnerabilityResult] = []
         vulns = response_json.get("vulnerabilities")
 
         # No `vulnerabilities` key means that there are no vulnerabilities for any version
--- test/service/test_pypi.py
+++ test/service/test_pypi.py
@@ -30,6 +30,18 @@ def test_pypi_multiple_pkg():
     assert len(results[deps[1]]) > 0
 
 
+def test_pypi_cached():
+    pypi = service.PyPIService()
+    dep = service.Dependency("jinja2", Version("2.4.1"))
+    results: List[service.VulnerabilityResult] = dict(pypi.query_all([dep]))
+    cached_results: List[service.VulnerabilityResult] = dict(pypi.query_all([dep]))
+    assert results == cached_results
+    assert len(results) == 1
+    assert dep in results
+    vulns = results[dep]
+    assert len(vulns) > 0
+
+
 def test_pypi_http_error(monkeypatch):
     def get_error_response():
         class MockResponse:
@@ -38,7 +50,7 @@ def test_pypi_http_error(monkeypatch):
 
         return MockResponse()
 
-    monkeypatch.setattr(requests, "get", lambda url: get_error_response())
+    monkeypatch.setattr(requests, "get", lambda url, headers: get_error_response())
 
     pypi = service.PyPIService()
     dep = service.Dependency("jinja2", Version("2.4.1"))
@@ -49,6 +61,9 @@ def test_pypi_http_error(monkeypatch):
 def test_pypi_mocked_response(monkeypatch):
     def get_mock_response():
         class MockResponse:
+            headers = {"ETag": "foo"}
+            status_code = 200
+
             def raise_for_status(self):
                 pass
 
@@ -65,7 +80,7 @@ def test_pypi_mocked_response(monkeypatch):
 
         return MockResponse()
 
-    monkeypatch.setattr(requests, "get", lambda url: get_mock_response())
+    monkeypatch.setattr(requests, "get", lambda url, headers: get_mock_response())
 
     pypi = service.PyPIService()
     dep = service.Dependency("foo", Version("1.0"))
@@ -83,6 +98,9 @@ def test_pypi_mocked_response(monkeypatch):
 def test_pypi_no_vuln_key(monkeypatch):
     def get_mock_response():
         class MockResponse:
+            headers = {"ETag": "foo"}
+            status_code = 200
+
             def raise_for_status(self):
                 pass
 
@@ -91,7 +109,7 @@ def test_pypi_no_vuln_key(monkeypatch):
 
         return MockResponse()
 
-    monkeypatch.setattr(requests, "get", lambda url: get_mock_response())
+    monkeypatch.setattr(requests, "get", lambda url, headers: get_mock_response())
 
     pypi = service.PyPIService()
     dep = service.Dependency("foo", Version("1.0"))
@@ -104,6 +122,9 @@ def test_pypi_no_vuln_key(monkeypatch):
 def test_pypi_invalid_version(monkeypatch):
     def get_mock_response():
         class MockResponse:
+            headers = {"ETag": "foo"}
+            status_code = 200
+
             def raise_for_status(self):
                 pass
 
@@ -120,9 +141,101 @@ def test_pypi_invalid_version(monkeypatch):
 
         return MockResponse()
 
-    monkeypatch.setattr(requests, "get", lambda url: get_mock_response())
+    monkeypatch.setattr(requests, "get", lambda url, headers: get_mock_response())
 
     pypi = service.PyPIService()
     dep = service.Dependency("foo", Version("1.0"))
     with pytest.raises(service.ServiceError):
         dict(pypi.query_all([dep]))
+
+
+def test_pypi_not_modified_response_without_cache(monkeypatch):
+    def get_mock_response():
+        class MockResponse:
+            status_code = 304
+
+            def raise_for_status(self):
+                pass
+
+        return MockResponse()
+
+    monkeypatch.setattr(requests, "get", lambda url, headers: get_mock_response())
+
+    pypi = service.PyPIService()
+    dep = service.Dependency("foo", Version("1.0"))
+    with pytest.raises(service.ServiceError):
+        dict(pypi.query_all([dep]))
+
+
+def test_pypi_cache_invalidation(monkeypatch):
+    class MockResponse:
+        headers = {"ETag": "foo"}
+        status_code = 200
+
+        def raise_for_status(self):
+            pass
+
+        def json(self):
+            return {
+                "vulnerabilities": [
+                    {
+                        "id": "VULN-0",
+                        "details": "The first vulnerability",
+                        "fixed_in": ["1.1", "1.4"],
+                    }
+                ]
+            }
+
+    def get_mock_response():
+        return MockResponse()
+
+    monkeypatch.setattr(requests, "get", lambda url, headers: get_mock_response())
+
+    pypi = service.PyPIService()
+    dep = service.Dependency("foo", Version("1.0"))
+    results: List[service.VulnerabilityResult] = dict(pypi.query_all([dep]))
+
+    assert len(results) == 1
+    assert dep in results
+    assert len(results[dep]) == 1
+    assert results[dep][0] == service.VulnerabilityResult(
+        id="VULN-0",
+        description="The first vulnerability",
+        fix_versions=[Version("1.1"), Version("1.4")],
+    )
+
+    # Simulate the case where our cache entry is invalidated.
+    def new_json():
+        return {
+            "vulnerabilities": [
+                {
+                    "id": "VULN-0",
+                    "details": "The first vulnerability",
+                    "fixed_in": ["1.1", "1.4"],
+                },
+                {
+                    "id": "VULN-1",
+                    "details": "The second vulnerability",
+                    "fixed_in": ["1.0"],
+                },
+            ]
+        }
+
+    MockResponse.json = lambda _: new_json()
+    new_results: List[service.VulnerabilityResult] = dict(pypi.query_all([dep]))
+    assert len(new_results) == 1
+    assert dep in new_results
+    assert len(new_results[dep]) == 2
+    print(new_results[dep])
+    assert new_results[dep] == [
+        service.VulnerabilityResult(
+            id="VULN-0",
+            description="The first vulnerability",
+            fix_versions=[Version("1.1"), Version("1.4")],
+        ),
+        service.VulnerabilityResult(
+            id="VULN-1",
+            description="The second vulnerability",
+            fix_versions=[Version("1.0")],
+        ),
+    ]
