57a0981093792adbb7439d2c13dd4af77380c79d
William Woodruff <william@trailofbits.com>
2021-11-08 13:54:38 -0500

    pyproject, pip_audit/service: document vuln service APIs

--- pip_audit/service/interface.py
+++ pip_audit/service/interface.py
@@ -1,3 +1,8 @@
+"""
+Interfaces for interacting with vulnerability services, i.e. sources
+of vulnerability information for fully resolved Python packages.
+"""
+
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Iterator, List, Tuple
@@ -8,34 +13,82 @@ from packaging.version import Version
 
 @dataclass(frozen=True)
 class Dependency:
+    """
+    Represents a fully resolved Python package.
+    """
+
     name: str
+    """
+    The package's **uncanonicalized** name.
+
+    Use the `canonicalized_name` property when a canonicalized form is necessary.
+    """
     version: Version
 
     # TODO(ww): Use functools.cached_property when supported Python is 3.8+.
     @property
     def canonical_name(self) -> str:
+        """
+        The `Dependency`'s PEP-503 canonicalized name.
+        """
         return canonicalize_name(self.name)
 
 
 @dataclass(frozen=True)
 class VulnerabilityResult:
+    """
+    Represents a "result" from a vulnerability service, indicating a vulnerability
+    in some Python package.
+    """
+
     id: str
+    """
+    A service-provided identifier for the vulnerability.
+    """
+
     description: str
+    """
+    A human-readable description of the vulnerability.
+    """
+
     fix_versions: List[Version]
+    """
+    A list of versions that can be upgraded to that resolve the vulnerability.
+    """
 
 
 class VulnerabilityService(ABC):
+    """
+    Represents an abstract provider of Python package vulnerability information.
+    """
+
     @abstractmethod
     def query(self, spec: Dependency) -> List[VulnerabilityResult]:  # pragma: no cover
+        """
+        Query the `VulnerabilityService` for information about the given `Dependency`,
+        returning a list of `VulnerabilityResult`.
+        """
         raise NotImplementedError
 
     def query_all(
         self, specs: Iterator[Dependency]
     ) -> Iterator[Tuple[Dependency, List[VulnerabilityResult]]]:
-        # Naive implementation that can be overridden if a particular service supports bulk queries
+        """
+        Query the vulnerability service for information on multiple dependencies.
+
+        `VulnerabilityService` implementations can override this implementation with
+        a more optimized one, if they support batched or bulk requests.
+        """
         for spec in specs:
             yield (spec, self.query(spec))
 
 
 class ServiceError(Exception):
+    """
+    Raised when a `VulnerabilityService` fails, for any reason.
+
+    Concrete implementations of `VulnerabilityService` are expected to subclass
+    this exception to provide more context.
+    """
+
     pass
