da4bc003bbd3549554204c610b6b013266eee7d1
Alex Cameron <asc@tetsuo.sh>
2021-11-30 03:24:13 +1100

    _state: Buffer logging while spinner is active (#137)
    Co-authored-by: William Woodruff <william.woodruff@trailofbits.com>

--- pip_audit/_state.py
+++ pip_audit/_state.py
@@ -3,9 +3,11 @@ Interfaces for for propagating feedback from the API to provide responsive progr
 well as a progress spinner implementation for use with CLI applications.
 """
 
+import logging
 import os
 from abc import ABC, abstractmethod
-from typing import Any, Dict
+from logging.handlers import MemoryHandler
+from typing import Any, Dict, List
 
 from progress.spinner import Spinner as BaseSpinner  # type: ignore
 
@@ -31,6 +33,15 @@ class AuditState(ABC):
         raise NotImplementedError  # pragma: no cover
 
     @abstractmethod
+    def initialize(self) -> None:
+        """
+        Called when `pip-audit`'s state is initializing. Implementors should override this to do
+        nothing if their state management requires no initialization step outside of the
+        constructor.
+        """
+        raise NotImplementedError  # pragma: no cover
+
+    @abstractmethod
     def finalize(self) -> None:
         """
         Called when `pip_audit`'s state is "done" changing. Implementors should
@@ -43,6 +54,8 @@ class AuditState(ABC):
         """
         Create an instance of the `pip-audit` state for usage within a `with` statement.
         """
+
+        self.initialize()
         return self
 
     def __exit__(self, _exc_type, _exc_value, _exc_traceback):  # pragma: no cover
@@ -72,6 +85,13 @@ class AuditSpinner(AuditState, BaseSpinner):  # pragma: no cover
         """
 
         super().__init__(message=message, **kwargs)
+
+        # Keep the target set to `None` to ensure that the logs don't get written until the spinner
+        # has finished writing output, regardless of the capacity argument
+        self.log_handler = MemoryHandler(
+            0, flushLevel=logging.ERROR, target=None, flushOnClose=False
+        )
+        self.prev_handlers: List[logging.Handler] = []
 
     def _writeln_truncated(self, line: str):
         """
@@ -111,8 +131,39 @@ class AuditSpinner(AuditState, BaseSpinner):  # pragma: no cover
         self.message = message
         self.next()
 
+    def initialize(self) -> None:
+        """
+        Redirect logging to an in-memory log handler so that it doesn't get mixed in with the
+        spinner output.
+        """
+        # Remove all existing log handlers
+        #
+        # We're recording them here since we'll want to restore them once the spinner falls out of
+        # scope
+        root_logger = logging.root
+        for handler in root_logger.handlers:
+            self.prev_handlers.append(handler)
+        for handler in self.prev_handlers:
+            root_logger.removeHandler(handler)
+
+        # Redirect logging to our in-memory handler that will buffer the log lines
+        root_logger.addHandler(self.log_handler)
+
     def finalize(self) -> None:
         """
-        Cleanup the spinner output so it doesn't get combined with subsequent `stderr` output.
+        Cleanup the spinner output so it doesn't get combined with subsequent `stderr` output and
+        flush any logs that were recorded while the spinner was active.
         """
         self.finish()
+
+        # Now that the spinner is complete, flush the logs
+        root_logger = logging.root
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
+        self.log_handler.setTarget(stream_handler)
+        self.log_handler.flush()
+
+        # Restore the original log handlers
+        root_logger.removeHandler(self.log_handler)
+        for handler in self.prev_handlers:
+            root_logger.addHandler(handler)
