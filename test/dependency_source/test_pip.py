8d3e8137fc25a84c077f17909b033bd6752671f1
William Woodruff <william@trailofbits.com>
2021-12-07 15:41:44 -0500

    test/dependency_source: remove a flaky assertion
    The assertion here is logically sound, but would commonly flake
    for PEBKAC reasons: users in a development environment might
    forget to `pip install .` between version bumps, causing the
    tests to see one `__version__` and the running code to see another.
    
    The assertion was a dupe of a much less flaky assertion, so removing
    it causes no coverage change.

--- test/dependency_source/test_pip.py
+++ test/dependency_source/test_pip.py
@@ -7,7 +7,6 @@ import pretend  # type: ignore
 import pytest
 from packaging.version import Version
 
-import pip_audit
 from pip_audit._dependency_source import pip
 from pip_audit._service.interface import ResolvedDependency, SkippedDependency
 
@@ -16,14 +15,11 @@ def test_pip_source():
     source = pip.PipSource()
 
     # We're running under pytest, so we can safely assume that pytest is in
-    # our execution environment. We're also running pip_audit itself, so we
-    # can safely test for ourselves.
+    # our execution environment.
     pytest_spec = ResolvedDependency(name="pytest", version=Version(pytest.__version__))
-    pip_audit_spec = ResolvedDependency(name="pip-audit", version=Version(pip_audit.__version__))
 
     specs = list(source.collect())
     assert pytest_spec in specs
-    assert pip_audit_spec in specs
 
 
 def test_pip_source_warns_about_old_pip(monkeypatch):
