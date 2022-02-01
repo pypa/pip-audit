a9e58d9c631c26ac7ba46294962a83c9a69dd960
Alex Cameron <asc@tetsuo.sh>
2022-02-02 00:46:02 +1100

    pypi: Check hashes against PyPI releases

--- pip_audit/_service/pypi.py
+++ pip_audit/_service/pypi.py
@@ -70,6 +70,31 @@ class PyPIService(VulnerabilityService):
         response_json = response.json()
         results: List[VulnerabilityResult] = []
 
+        # If the dependency has a hash explicitly listed, check it against the PyPI data
+        if spec.hash is not None:
+            releases = response_json["releases"]
+            release = releases.get(str(spec.version))
+            if release is None:
+                raise ServiceError(
+                    "Could not find release to compare hashes: "
+                    f"{spec.canonical_name} ({spec.version})"
+                )
+            found = False
+            hash_type, hash_value = spec.hash.split(":", 1)
+            for dist in release:
+                digests = dist.get("digests")
+                if digests is None:
+                    continue
+                pypi_hash = digests.get(hash_type)
+                if pypi_hash is not None and pypi_hash == hash_value:
+                    found = True
+                    break
+            if not found:
+                raise ServiceError(
+                    f"Mismatched hash for {spec.canonical_name} ({spec.version}): listed "
+                    f"{hash_value} of type {hash_type} could not be found in PyPI releases"
+                )
+
         vulns = response_json.get("vulnerabilities")
 
         # No `vulnerabilities` key means that there are no vulnerabilities for any version
