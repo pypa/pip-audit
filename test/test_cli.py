38ef6973e181bc12b6986c521693df2570994746
Adrian Torres <atorresj@redhat.com>
2022-05-10 17:15:20 +0200

    cli: allow ignoring specific vulnerability IDs
    This commit introduces a new flag to pip-audit's CLI (--ignore-vuln)
    that takes a string representing a vulnerability id, if any of the
    packages scanned detect said vulnerability, it will be ignored in the
    final report.
    
    This allows users of pip-audit to ignore certain vulnerabilities that
    they may deem not exploitable for whatever reason.
    
    The flag can be used multiple times to ignore multiple vulnerabilities.
    
    Closes #245

--- test/test_cli.py
+++ test/test_cli.py
@@ -10,9 +10,12 @@ import pip_audit._cli
         ([], 1, 1, "Found 1 known vulnerability in 1 package"),
         ([], 2, 1, "Found 2 known vulnerabilities in 1 package"),
         ([], 2, 2, "Found 2 known vulnerabilities in 2 packages"),
+        (["--ignore-vuln", "bar"], 2, 2, "Found 2 known vulnerabilities, ignored 1 in 2 packages"),
         (["--fix"], 1, 1, "fixed 1 vulnerability in 1 package"),
         (["--fix"], 2, 1, "fixed 2 vulnerabilities in 1 package"),
         (["--fix"], 2, 2, "fixed 2 vulnerabilities in 2 packages"),
+        ([], 0, 0, "No known vulnerabilities found"),
+        (["--ignore-vuln", "bar"], 0, 1, "No known vulnerabilities found, 1 ignored"),
     ],
 )
 def test_plurals(capsys, monkeypatch, args, vuln_count, pkg_count, expected):
@@ -30,10 +33,14 @@ def test_plurals(capsys, monkeypatch, args, vuln_count, pkg_count, expected):
                 canonical_name="something" + str(i),
                 version=1,
             ),
-            [pretend.stub(fix_versions=[2], id="foo")] * (vuln_count // pkg_count),
+            [pretend.stub(fix_versions=[2], id="foo", aliases=set(), has_any_id=lambda x: False)]
+            * (vuln_count // pkg_count),
         )
         for i in range(pkg_count)
     ]
+
+    if "--ignore-vuln" in args:
+        result[0][1].append(pretend.stub(id="bar", aliases=set(), has_any_id=lambda x: True))
 
     auditor = pretend.stub(audit=lambda a: result)
     monkeypatch.setattr(pip_audit._cli, "Auditor", lambda *a, **kw: auditor)
