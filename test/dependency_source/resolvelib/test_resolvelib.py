da73c63ba4580b4c1da4b0ad7e6a07ef04ef5dc5
William Woodruff <william@yossarian.net>
2023-01-19 20:49:05 -0500

    test: fixups for exception changes

--- test/dependency_source/resolvelib/test_resolvelib.py
+++ test/dependency_source/resolvelib/test_resolvelib.py
@@ -9,10 +9,10 @@ from packaging.requirements import Requirement
 from packaging.version import Version
 from pip_api import Requirement as ParsedRequirement
 from requests.exceptions import HTTPError
-from resolvelib.resolvers import ResolutionImpossible
 
 from pip_audit._dependency_source import resolvelib
 from pip_audit._dependency_source.resolvelib import pypi_provider
+from pip_audit._dependency_source.resolvelib.resolvelib import ResolveLibResolverError
 from pip_audit._service.interface import ResolvedDependency, SkippedDependency
 
 
@@ -197,7 +197,7 @@ def test_resolvelib_sdist_vexing_parse(monkeypatch):
     )
 
     req = Requirement("cffi")
-    with pytest.raises(ResolutionImpossible):
+    with pytest.raises(ResolveLibResolverError):
         dict(resolver.resolve_all(iter([req])))
 
 
@@ -218,7 +218,7 @@ def test_resolvelib_wheel_python_version(monkeypatch):
     )
 
     req = Requirement("flask==2.0.1")
-    with pytest.raises(ResolutionImpossible):
+    with pytest.raises(ResolveLibResolverError):
         dict(resolver.resolve_all(iter([req])))
 
 
@@ -236,7 +236,9 @@ def test_resolvelib_wheel_python_version_invalid_specifier(monkeypatch):
         'data-requires-python="&lt;=3.5.*">Flask-2.0.1-py3-none-any.whl</a><br/>'
     )
 
-    logger = pretend.stub(warning=pretend.call_recorder(lambda s: None))
+    logger = pretend.stub(
+        warning=pretend.call_recorder(lambda s: None), debug=pretend.call_recorder(lambda s: None)
+    )
     monkeypatch.setattr(pypi_provider, "logger", logger)
 
     monkeypatch.setattr(
@@ -260,7 +262,7 @@ def test_resolvelib_wheel_python_version_invalid_specifier(monkeypatch):
 
 def test_resolvelib_wheel_canonical_name_mismatch(monkeypatch):
     # Call the underlying wheel, Mask instead of Flask. This should throw an `ResolutionImpossible`
-    # error.
+    # error, which we'll surface as a `ResolveLibResolverError`.
     data = (
         '<a href="https://files.pythonhosted.org/packages/54/4f/'
         "1b294c1a4ab7b2ad5ca5fc4a9a65a22ef1ac48be126289d97668852d4ab3/Mask-2.0.1-py3-none-any.whl#"
@@ -278,7 +280,7 @@ def test_resolvelib_wheel_canonical_name_mismatch(monkeypatch):
     )
 
     req = Requirement("flask==2.0.1")
-    with pytest.raises(ResolutionImpossible):
+    with pytest.raises(ResolveLibResolverError):
         dict(resolver.resolve_all(iter([req])))
 
 
@@ -302,7 +304,7 @@ def test_resolvelib_wheel_invalid_version(monkeypatch):
     )
 
     req = Requirement("flask==2.0.1")
-    with pytest.raises(ResolutionImpossible):
+    with pytest.raises(ResolveLibResolverError):
         dict(resolver.resolve_all(iter([req])))
 
 
@@ -320,7 +322,7 @@ def test_resolvelib_sdist_invalid_suffix(monkeypatch):
     )
 
     req = Requirement("flask==2.0.1")
-    with pytest.raises(ResolutionImpossible):
+    with pytest.raises(ResolveLibResolverError):
         dict(resolver.resolve_all(iter([req])))
 
 
